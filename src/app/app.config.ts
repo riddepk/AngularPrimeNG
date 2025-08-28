d696df1d0fcba7db2f09bbc86ee74f343ecf00cd
Patrick <p.derider@stag.technofuturtic.education>
2025-08-28 21:37:17 +0200

    add boutons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import Aura from '@primeuix/themes/aura'
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
+    providePrimeNG({
+      theme:{
+        preset:Aura
+      }
+    })
   ]
 };
--- src/app/layout/pages/nav/nav.ts
+++ src/app/layout/pages/nav/nav.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
+import { ButtonModule  } from 'primeng/button';
+import { ButtonGroupModule } from 'primeng/buttongroup';
+
 
 @Component({
   selector: 'app-nav',
-  imports: [ButtonModule],
+  imports: [ButtonModule,ButtonGroupModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
