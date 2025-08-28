b3d223fb65d5654c4e9531b761097e08fbf77288
Patrick <p.derider@stag.technofuturtic.education>
2025-08-28 22:23:16 +0200

    Ajout Login

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {Button} from 'primeng/button';
+import {FloatLabel} from 'primeng/floatlabel';
+import {InputText} from 'primeng/inputtext';
+import {Password} from 'primeng/password';
+import {AutoFocus} from 'primeng/autofocus';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    Button,
+    FloatLabel,
+    FormsModule,
+    InputText,
+    Password,
+    ReactiveFormsModule,
+    AutoFocus
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
 
+  private readonly _authService: AuthService = inject(AuthService);
+  private readonly _fb: FormBuilder = inject(FormBuilder);
+  private readonly _router: Router = inject(Router);
+
+  loginForm: FormGroup;
+
+  constructor() {
+    this.loginForm = this._fb.group({
+      username: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+    });
+  }
+
+  submit(): void {
+    this.loginForm.markAllAsTouched();
+
+    if(this.loginForm.valid) {
+      this._authService.login(this.loginForm.value).subscribe({
+        next: (result) => {
+          console.log(result);
+          this._router.navigateByUrl('/home');
+        },
+        error: err => {
+          console.log(err);
+        }
+      });
+    }
+  }
 }
