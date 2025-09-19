1d3e246b5ba815eec5473bec01bdbe784ff3a029
Patrick DE RIDDER <riddepk@gmail.com>
2025-09-19 20:51:31 +0200

    commit

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http'; // Si nécessaire
+import { MqttModule, IMqttServiceOptions } from 'ngx-mqtt';
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
